dc6e4569634e09ff4737e29a9a265dc66e8f567b
XFury007 <grevematthew@gmail.com>
2025-07-14 16:14:30 +0200

    toptab buttons

--- Assignment8/Assignment8/BottomBar/Home.xaml.cs
+++ Assignment8/Assignment8/BottomBar/Home.xaml.cs
@@ -1,5 +1,5 @@
 
-
+using Assignment8.TopBar; 
 namespace Assignment8.BottomBar;
 
 public partial class Home : ContentPage
@@ -8,4 +8,12 @@ public partial class Home : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void OnGoToAnimePageClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Anime());
+    }
+    private async void OnGoToConceptArtPageClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ConceptArt());
+    }
 }
